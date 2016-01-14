cf01ed53bab23171b67123e432dc5ba4994b4d59
yinzhifang <yinzhifang5@163.com>
2016-01-14 18:11:41 +0800

    3222

--- myTest/src/main/java/com/yzf/test/Test.java
+++ myTest/src/main/java/com/yzf/test/Test.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
-package com.yzf.test;
 
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
 	}
 }
->>>>>>> branch 'master' of https://github.com/yinzhifang/test.git
+
